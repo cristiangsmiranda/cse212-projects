63e1895ffc86c37f3685a2eac2c38c4da7d61363
cristiangsmiranda <san24063@byui.edu>
2025-09-29 23:20:59 -0300

    W05 Code: Recursion added

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -15,7 +15,10 @@ public static class Recursion
     public static int SumSquaresRecursive(int n)
     {
         // TODO Start Problem 1
-        return 0;
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        return n * n + SumSquaresRecursive(n - 1);
+
     }
 
     /// <summary>
@@ -40,6 +43,18 @@ public static class Recursion
     public static void PermutationsChoose(List<string> results, string letters, int size, string word = "")
     {
         // TODO Start Problem 2
+        if (word.Length == size)
+        {
+            results.Add(word);
+            return;
+        }
+        if (letters.Length == 0) return;
+        for (int i = 0; i < letters.Length; i++)
+        {
+            char c = letters[i];
+            string newLetters = letters.Remove(i, 1);
+            PermutationsChoose(results, newLetters, size, word + c);
+        }
     }
 
     /// <summary>
@@ -87,6 +102,8 @@ public static class Recursion
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
         // Base Cases
+        if (remember == null)
+            remember = new Dictionary<int, decimal>();
         if (s == 0)
             return 0;
         if (s == 1)
@@ -98,8 +115,13 @@ public static class Recursion
 
         // TODO Start Problem 3
 
+        if (remember.ContainsKey(s))
+            return remember[s];
+
         // Solve using recursion
-        decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+        decimal ways = CountWaysToClimb(s - 1, remember) + CountWaysToClimb(s - 2, remember) + CountWaysToClimb(s - 3, remember);
+        
+        remember[s] = ways;
         return ways;
     }
 
@@ -119,6 +141,19 @@ public static class Recursion
     public static void WildcardBinary(string pattern, List<string> results)
     {
         // TODO Start Problem 4
+        int index = pattern.IndexOf('*');
+        if (index == -1)
+        {
+            results.Add(pattern);
+            return;
+        }
+
+        string pattern0 = pattern.Substring(0, index) + '0' + pattern.Substring(index + 1);
+        WildcardBinary(pattern0, results);
+
+        string pattern1 = pattern.Substring(0, index) + '1' + pattern.Substring(index + 1);
+        WildcardBinary(pattern1, results);
+
     }
 
     /// <summary>
@@ -129,15 +164,34 @@ public static class Recursion
     {
         // If this is the first time running the function, then we need
         // to initialize the currPath list.
-        if (currPath == null) {
+        if (currPath == null)
             currPath = new List<ValueTuple<int, int>>();
+
+        currPath.Add((x, y));
+
+        if (maze.IsEnd(x, y))
+        {
+            results.Add(currPath.AsString());
         }
-        
+        else
+        {
+            if (maze.IsValidMove(currPath, x + 1, y))
+                SolveMaze(results, maze, x + 1, y, new List<(int, int)>(currPath));
+
+            if (maze.IsValidMove(currPath, x - 1, y))
+                SolveMaze(results, maze, x - 1, y, new List<(int, int)>(currPath));
+
+            if (maze.IsValidMove(currPath, x, y + 1))
+                SolveMaze(results, maze, x, y + 1, new List<(int, int)>(currPath));
+
+            if (maze.IsValidMove(currPath, x, y - 1))
+                SolveMaze(results, maze, x, y - 1, new List<(int, int)>(currPath));
+        }
         // currPath.Add((1,2)); // Use this syntax to add to the current path
 
-        // TODO Start Problem 5
-        // ADD CODE HERE
+            // TODO Start Problem 5
+            // ADD CODE HERE
 
-        // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
+            // results.Add(currPath.AsString()); // Use this to add your path to the results array keeping track of complete maze solutions when you find the solution.
     }
 }
